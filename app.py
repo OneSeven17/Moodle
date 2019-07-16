c55abf1a8ea58f5cff13adf4d66ed08fa95dfd7b
OneSeven17 <17renatis@gmail.com>
2019-07-16 11:59:14 +0300

    css fixed

--- app.py
+++ app.py
@@ -2,14 +2,18 @@ import os
 
 import waitress
 from flask import Flask, render_template, redirect, url_for, request
+from werkzeug.utils import secure_filename
 
 import db
 
 
 def start():
     DATABASE_URL = 'db.sqlite'
+    UPLOAD_FOLDER = '/'
+    ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp4'])
 
     app = Flask(__name__)
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
     @app.route('/')
     def index():
@@ -46,6 +50,10 @@ def start():
     def movie_new():
         return render_template('movie-add.html')
 
+    def allowed_file(filename):
+        return '.' in filename and \
+               filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
     @app.route('/new/add', methods=['POST'])
     def movie_add():
         name = request.form['name']
@@ -54,6 +62,13 @@ def start():
         tags = request.form['tags']
         poster = request.form['poster']
         movie = db.add_movie(DATABASE_URL, name, category, genre, tags, poster)
+        # if request.method == 'POST':
+        #     file = request.files['file']
+        #     if file and allowed_file(file.filename):
+        #         filename = secure_filename(file.filename)
+        #         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #         return redirect(url_for('movie_details',
+        #                                 id=movie, filename=filename))
         return redirect(url_for('movie_details', id=movie))
 
     @app.route('/<id>/remove', methods=['POST'])
@@ -67,6 +82,18 @@ def start():
         search = request.form.get("action")
         results = db.search_movies_by_tags(DATABASE_URL, search)
         return render_template('movie-search.html', movies=results, search=search)
+
+
+    # @app.route('/upload')
+    # def upload_file():
+    #     return render_template('movie-add.html')
+    #
+    # @app.route('/uploader', methods=['GET', 'POST'])
+    # def upload_file():
+    #     if request.method == 'POST':
+    #         f = request.files['file']
+    #         f.save(secure_filename(f.filename))
+    #         return 'file uploaded successfully'
 
     print(os.getenv('APP_ENV'))
     print(os.getenv('PORT'))
